2ad94a785c86053318023555ae1e45d8dc6a88b7
Yiyuan Zhang <56832141+eavan620@users.noreply.github.com>
2021-08-19 10:26:54 +0200

    Add files via upload

---
+++ Get_Mean_Number2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+scores1 = [91,95,97,99,92,93,96,98]
+scores2 = []
+
+average = np.mean(scores1)
+print(f"The mean number is: {average}")
+
+for score in scores1:
+	if score < average:
+		scores2.append(score)
+print(f"Below the average scores are: {scores2}")
+
+scores3 = np.array(scores1)
+print(f"Below the average are: {scores3[scores3 < average]}")
