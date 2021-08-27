83728973bd9ee986b656d6a484eaeb26a4c6e72f
Yiyuan Zhang <56832141+eavan620@users.noreply.github.com>
2021-08-27 20:11:39 +0200

    Add files via upload
    Create a function to find a lucky dog from a list

---
+++ luckydog.py
@@ -0,0 +1,24 @@
+import random, time 
+
+def luckydog(a,b,c):
+	luckylists = [a, b, c]
+	a = random.choice(luckylists)
+	print("Who is the lucky dog?")
+	time.sleep(1)
+	print("Countdown: 3")
+	time.sleep(1)
+	print("Countdown: 2")
+	time.sleep(1)
+	print("Countdown: 1")
+	time.sleep(1)
+	image = '''
+	 /\_)o<
+	|      \\
+	| O . O|
+	 \_____/
+	'''
+	print(image)
+	print(f"Congrats {a} got the prize!")
+
+luckydog("benben","boniu","niuniu")
+
