775ea70c56b2d4b36cb0a5aa0fef0b4bd3100408
Yiyuan Zhang <56832141+eavan620@users.noreply.github.com>
2021-08-27 19:54:15 +0200

    Add files via upload
    Create two functions to get a person's bonus which depends on the working time.

---
+++ Bonus.py
@@ -0,0 +1,16 @@
+def bonus(time):
+
+	if time < 6:
+		money = 500
+	elif 6 < time <= 12:
+		money = 120 * time 
+	else:
+		money = 180 * time 
+	return money
+
+def decison(name, time):
+	gain = bonus(time)
+	print(f"{name} worked {time} months here and the bonus is {gain} dollor.")
+
+decison("Yiyuan",24)
+
